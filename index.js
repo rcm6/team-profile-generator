7aeb574492a65911b027a58e35f98c0adb667904
Russell Coleman <rootofsilence@gmail.com>
2023-02-26 13:59:53 +0000

    updated manager/intern/engineer js, added functions to index.js to call questions

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const render = require("./src/page-template.js");
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
 //manager questions
-const mnanagerQuestions = [
+const managerQuestions = [
     {
       type: "input",
       name: "name",
@@ -94,3 +94,40 @@ const menuQuestions = [
         choices: ['Add an engineer', 'Add an intern', "Finish building the team"]
       }
 ];
+
+//function to initialize program
+function init() {
+    console.log('init');
+    //ask Manager Questions
+    addManager();
+}
+
+//function to add manager
+function addManager() {
+    console.log('add manager');
+    inquirer.prompt(managerQuestions);
+}
+
+//function to display menu
+function displayMenu() {
+    console.log('display menu');
+    inquirer.prompt(menuQuestions);
+}
+
+//function to add intern
+function addIntern() {
+    console.log('add intern');
+    inquirer.prompt(internQuestions);
+}
+
+//function to add engineer
+function addEngineer() {
+    console.log('add engineer');
+    inquirer.prompt(engineerQuestions);
+}
+
+
+
+
+//function call to initialize program
+init();
