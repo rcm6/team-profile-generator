e02ffd6c0cff83540a069ddd9b9004d4ed2ea6e5
Russell Coleman <rootofsilence@gmail.com>
2023-02-26 13:21:07 +0000

    updated index.js - added questions

--- index.js
+++ index.js
@@ -13,3 +13,84 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+//manager questions
+const mnanagerQuestions = [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the manager's name?"
+    }, 
+    {
+      type: "input",
+      name: "id",
+      message: "What is the manager's employee id?"
+    },        
+    {
+      type: "input",
+      name: "email",
+      message: "What is the manager's email address?"
+    },        
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the manager's office number?"
+    }
+];
+
+//engineer questions
+const engineerQuestions = [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the engineer's name?"
+    }, 
+    {
+      type: "input",
+      name: "id",
+      message: "What is the engineer's employee id?"
+    },        
+    {
+      type: "input",
+      name: "email",
+      message: "What is the engineer's email address?"
+    },        
+    {
+      type: "input",
+      name: "github",
+      message: "What is the engineer's GitHub username?"
+    }
+];
+
+//intern questions
+const internQuestions = [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the intern's name?"
+    }, 
+    {
+      type: "input",
+      name: "id",
+      message: "What is the intern's employee id?"
+    },        
+    {
+      type: "input",
+      name: "email",
+      message: "What is the intern's email address?"
+    },        
+    {
+      type: "input",
+      name: "school",
+      message: "What is the intern's school?"
+    }
+];
+
+//menu questions
+const menuQuestions = [
+    {
+        type: "rawlist",
+        name: "menu",
+        message: "What would you like to do next?",
+        choices: ['Add an engineer', 'Add an intern', "Finish building the team"]
+      }
+];
